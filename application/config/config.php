0a6f8e2fd4e9ac5cc196d000d0536c2780b3a47d
MFDonadeli <mfdonadeli@gmail.com>
2017-07-07 16:19:08 -0300

    - Remover index.php da URL

--- application/config/config.php
+++ application/config/config.php
@@ -37,7 +37,7 @@ $config['base_url'] = 'http://mfdigital.com.br/metricas/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
