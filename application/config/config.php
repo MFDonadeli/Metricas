da6b96e224c42695824cabdec8f8779515b23c10
MFDonadeli <mfdonadeli@gmail.com>
2017-07-07 10:50:30 -0300

    - Configuração de URL para funcionar no servidor

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/~mfdonadeli/metricas/';
+//$config['base_url'] = 'http://localhost/~mfdonadeli/metricas/';
+$config['base_url'] = 'http://45.56.88.225/metricas/';
+
 
 /*
 |--------------------------------------------------------------------------
