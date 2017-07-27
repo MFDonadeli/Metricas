65540c6532a48658a75b999fb48a21d9e79b3000
MFDonadeli <mfdonadeli@gmail.com>
2017-07-26 23:10:23 -0300

    - Acertando paths do template

--- application/views/metricas/lib/config.php
+++ application/views/metricas/lib/config.php
@@ -11,7 +11,7 @@ if(strpos($directory, $document_root)===0) {
     $base_url .= str_replace(DIRECTORY_SEPARATOR, '/', substr($directory, strlen($document_root)));
 }
 
-defined("APP_URL") ? null : define("APP_URL", str_replace("/lib", "", $base_url));
+defined("APP_URL") ? null : define("APP_URL", $base_url);
 //Assets URL, location of your css, img, js, etc. files
 defined("ASSETS_URL") ? null : define("ASSETS_URL", APP_URL);
 
