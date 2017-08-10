cb06c865cf0d0e3e556f8df83a0008e6b99aca98
MFDonadeli <mfdonadeli@gmail.com>
2017-08-10 18:51:55 -0300

    login as other user

--- application/views/laou.php
+++ application/views/laou.php
@@ -1,32 +1,11 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    </head>
     <body>
+        <form action='app/fkhome' method='post'>
         <select name="cmbprofiles" id="cmbprofiles">
             <?php echo $retorno; ?>
         </select>
-        <button id='btnLogin'>Login</button>
+        <input type="submit" id='btnLogin' value="Login"/>
+        </form>
     </body>
 
-    <script>
-        $('#btnLogin').click(function(){
-            var form_data = {
-                usr_fk_home: $('#cmbprofiles').val()
-            };
-
-            var resp = $.ajax({
-                url: 'app/fkhome',
-                type: 'POST',
-                data: form_data,
-                global: false,
-                async:false,
-                success: function(msg) { 
-             
-                }
-            });
-        });
-    </script>
 </html>
