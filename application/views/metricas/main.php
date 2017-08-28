9ff9c2e629e3806533c5ef30385b60fbb368ffc8
MFDonadeli <mfdonadeli@gmail.com>
2017-08-27 23:12:54 -0300

    - [DEV] Grafico no main

--- application/views/metricas/main.php
+++ application/views/metricas/main.php
@@ -1,4 +1,58 @@
 <?php require_once("inc/init.php"); ?>
+<head>
+		<meta charset="utf-8">
+		<!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+
+		<title>Início</title>
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<!-- Basic Styles -->
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/font-awesome.min.css">
+
+		<!-- SmartAdmin Styles : Caution! DO NOT change the order -->
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/smartadmin-production-plugins.min.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/smartadmin-production.min.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/smartadmin-skins.min.css">
+
+		<!-- SmartAdmin RTL Support is under construction-->
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/smartadmin-rtl.min.css">
+
+		<!-- We recommend you use "your_style.css" to override SmartAdmin
+		     specific styles this will also ensure you retrain your customization with each SmartAdmin update.
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/your_style.css"> -->
+
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/styles.css">
+
+		<!-- Demo purpose only: goes with demo.js, you can delete this css when designing your own WebApp -->
+		<link rel="stylesheet" type="text/css" media="screen" href="http://superadmetrics.com/metricas/assets/css/demo.min.css">
+
+		<!-- FAVICONS -->
+		<link rel="shortcut icon" href="http://superadmetrics.com/metricas/assets/img/favicon/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="http://superadmetrics.com/metricas/assets/img/favicon/favicon.ico" type="image/x-icon">
+
+		<!-- GOOGLE FONT -->
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+
+		<!-- Specifying a Webpage Icon for Web Clip
+			 Ref: https://developer.apple.com/library/ios/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html -->
+		<link rel="apple-touch-icon" href="http://superadmetrics.com/metricas/assets/img/splash/sptouch-icon-iphone.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="http://superadmetrics.com/metricas/assets/img/splash/touch-icon-ipad.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="http://superadmetrics.com/metricas/assets/img/splash/touch-icon-iphone-retina.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="http://superadmetrics.com/metricas/assets/img/splash/touch-icon-ipad-retina.png">
+
+		<!-- iOS web-app metas : hides Safari UI Components and Changes Status Bar Appearance -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+		<!-- Startup image for web apps -->
+		<link rel="apple-touch-startup-image" href="http://superadmetrics.com/metricas/assets/img/splash/ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)">
+		<link rel="apple-touch-startup-image" href="http://superadmetrics.com/metricas/assets/img/splash/ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)">
+		<link rel="apple-touch-startup-image" href="http://superadmetrics.com/metricas/assets/img/splash/iphone.png" media="screen and (max-device-width: 320px)">
+	<style type="text/css">.jqstooltip { position: absolute;left: 0px;top: 0px;visibility: hidden;background: rgb(0, 0, 0) transparent;background-color: rgba(0,0,0,0.6);filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)";color: white;font: 10px arial, san serif;text-align: left;white-space: nowrap;padding: 5px;border: 1px solid white;z-index: 10000;}.jqsfield { color: white;font: 10px arial, san serif;text-align: left;}</style></head>
 
 <div class="row">
 	<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
